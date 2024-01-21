8768c498d55210c42bf4e9dada88c333cf5c1165
chedesubramanyasai <157195117+chedesubramanyasai@users.noreply.github.com>
2024-01-21 12:17:18 -0600

    Add files via upload

---
+++ string functions.py
@@ -0,0 +1,9 @@
+print('Enter a string:')
+st = str(input())
+
+print('Length of given string is: ',len(st))
+print('Upper case of given string: ',st.upper())
+print('Lower case of given string: ',st.lower())
+print('Reversed given string: ',st[::-1])
+
+
