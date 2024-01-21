86a2e8d53696793e69ef52dc94d15c1a0c3c5b13
chedesubramanyasai <157195117+chedesubramanyasai@users.noreply.github.com>
2024-01-21 12:25:56 -0600

    Add files via upload

---
+++ String Formatting.py
@@ -0,0 +1,10 @@
+print('Name: ')
+name= str(input())
+print('Age: ')
+age = int(input())
+print('Fav color: ')
+favc = str(input())
+
+
+print('Hello, {0} You are {1} years old, and your favorite color is {2}.'.format(name, age, favc))
+
